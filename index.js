7e4e6deca27bf3343fb10dc9fa17a0e04d2c6e18
jkgyso <jkgyso@gmail.com>
2024-07-10 20:15:38 +0800

    added cors

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const mongoose = require('mongoose');
+const cors = require("cors");
 
 const app = express();
 
@@ -7,6 +8,7 @@ const port = 4000;
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
+app.use(cors());
 
 mongoose.connect('mongodb+srv://admin:admin1234@gysodb.nijqxz1.mongodb.net/Fitness-App?retryWrites=true&w=majority&appName=gysoDB');
 
